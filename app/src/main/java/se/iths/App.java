757903d800fed5ebe5ef67a9cd9a37eaee15f46b
Ken Laptop <samuel.ken.segawa@gmail.com>
2022-12-23 21:42:18 +0100

    Added exception handlers

--- app/src/main/java/se/iths/App.java
+++ app/src/main/java/se/iths/App.java
@@ -21,14 +21,28 @@ public class App {
     private static final String SQL_COL_ARTIST_ID = "ArtistId";
     private static final String SQL_COL_ARTIST_NAME = "Name";
     public static void main(String[] args) throws SQLException {
-        Connection con = DriverManager.getConnection(JDBC_CONNECTION, JDBC_USER, JDBC_PASSWORD);
-        ResultSet rs = con.createStatement().executeQuery(SQL_SELECT_ALL_ARTISTS);
-        while (rs.next()) {
-            long id = rs.getLong(SQL_COL_ARTIST_ID);
-            String name = rs.getString(SQL_COL_ARTIST_NAME);
-            artists.add(new Artist(id, name));
+        Connection con = null;
+        ResultSet rs = null;
+        try {
+            con = DriverManager.getConnection(JDBC_CONNECTION, JDBC_USER, JDBC_PASSWORD);
+            rs = con.createStatement().executeQuery(SQL_SELECT_ALL_ARTISTS);
+            while (rs.next()) {
+                long id = rs.getLong(SQL_COL_ARTIST_ID);
+                String name = rs.getString(SQL_COL_ARTIST_NAME);
+                artists.add(new Artist(id, name));
+            }
+        } catch (SQLException e) {
+            System.err.println(String.format("Error reading database %s", e.toString()));
+        } finally {
+            try {
+                rs.close();
+            } catch (Exception ignore) {}
+            try {
+                con.close();
+            } catch (Exception ignore) {}
         }
-        rs.close();
-        con.close();
+        for(Artist artist : artists) {
+            System.out.println(artist);
+        }
     }
 }
