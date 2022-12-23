2fff87a7121fb060ebcfcd2a8be01607ab80e659
Ken Laptop <samuel.ken.segawa@gmail.com>
2022-12-23 11:02:38 +0100

    Updated normalisering script

--- normalisering.sql
+++ normalisering.sql
@@ -1,3 +1,7 @@
+DROP DATABASE IF EXISTS iths;
+
+CREATE DATABASE iths;
+
 USE iths;
 
 DROP TABLE IF EXISTS UNF;
@@ -25,7 +29,63 @@ IGNORE 1 ROWS;
 
 DROP TABLE IF EXISTS Student;
 
-CREATE TABLE Student (Id INT NOT NULL AUTO_INCREMENT, FirstName VARCHAR(255) NOT NULL, LastName VARCHAR(255) NOT NULL, CONSTRAINT PRIMARY KEY (Id)) ENGINE=INNODB;
+CREATE TABLE Student (
+	Id INT NOT NULL AUTO_INCREMENT,
+	FirstName VARCHAR(255) NOT NULL,
+	LastName VARCHAR(255) NOT NULL,
+	CONSTRAINT PRIMARY KEY (Id)
+) ENGINE=INNODB;
 
 INSERT INTO Student (Id, FirstName, LastName) SELECT DISTINCT Id, SUBSTRING_INDEX(Name, ' ', 1) AS FirstName, SUBSTRING_INDEX(Name, ' ', -1) AS LastName FROM UNF;
 
+DROP TABLE IF EXISTS Phone;
+
+CREATE TABLE Phone (
+    PhoneId INT NOT NULL AUTO_INCREMENT,
+    StudentId INT NOT NULL,
+    Type VARCHAR(32),
+    Number VARCHAR(32) NOT NULL,
+    CONSTRAINT PRIMARY KEY(PhoneId)
+) ENGINE=INNODB;
+
+INSERT INTO Phone(StudentId, Type, Number) 
+SELECT Id As StudentId, "Home" AS Type, HomePhone as Number FROM UNF
+WHERE HomePhone IS NOT NULL AND HomePhone != ''
+UNION SELECT Id As StudentId, "Job" AS Type, JobPhone as Number FROM UNF
+WHERE JobPhone IS NOT NULL AND JobPhone != ''
+UNION SELECT Id As StudentId, "Mobile" AS Type, MobilePhone1 as Number FROM UNF
+WHERE MobilePhone1 IS NOT NULL AND MobilePhone1 != ''
+UNION SELECT Id As StudentId, "Mobile" AS Type, MobilePhone2 as Number FROM UNF
+WHERE MobilePhone2 IS NOT NULL AND MobilePhone2 != ''
+;
+DROP VIEW IF EXISTS PhoneList;
+
+CREATE VIEW PhoneList AS SELECT StudentId, group_concat(Number) FROM Phone GROUP BY StudentId;
+
+CREATE TABLE School (
+	SchoolId INT NOT NULL AUTO_INCREMENT,
+	Name VARCHAR(255) NOT NULL,
+	City VARCHAR(255) NOT NULL,
+	CONSTRAINT PRIMARY KEY (SchoolId)
+) ENGINE=INNODB;
+
+INSERT INTO School(Name, City) SELECT DISTINCT School, City FROM UNF;
+
+DROP TABLE IF EXISTS StudentSchool;
+
+CREATE TABLE StudentSchool AS SELECT Id AS StudentId, SchoolId FROM UNF JOIN School ON UNF.School = School.Name;
+
+CREATE TABLE Hobby (
+	HobbyId INT NOT NULL AUTO_INCREMENT,
+	StudentId INT NOT NULL,
+	Category VARCHAR(32),
+	CONSTRAINT PRIMARY KEY (HobbyId)
+) ENGINE=INNODB;
+
+INSERT INTO Hobby (StudentId, Category)
+SELECT Id AS StudentId, trim(SUBSTRING_INDEX(Hobbies, ',', 1)) AS Category FROM UNF
+WHERE Hobbies IS NOT NULL AND Hobbies != ''
+UNION SELECT Id AS StudentId, trim(SUBSTRING_INDEX(SUBSTRING_INDEX(HOBBIES, ',', -2), ',', 1)) AS Category FROM UNF
+WHERE Hobbies IS NOT NULL AND Hobbies != ''
+UNION SELECT Id AS StudentId, trim(SUBSTRING_INDEX(Hobbies, ',', -1)) AS Category FROM UNF
+WHERE Hobbies IS NOT NULL AND Hobbies != '';
