28de12cc3a0c23a34301a2b000d75ec9df760e68
Ken Laptop <samuel.ken.segawa@gmail.com>
2022-12-23 22:15:27 +0100

    Updated Artist class

--- app/src/main/java/se/iths/Artist.java
+++ app/src/main/java/se/iths/Artist.java
@@ -1,8 +1,12 @@
 package se.iths;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Artist {
     private final long id;
     private String name;
+    private Collection<Album> albums = new ArrayList<>();
 
     public Artist(long id, String name) {
         this.id = id;
@@ -21,8 +25,21 @@ public class Artist {
         this.name = name;
     }
 
+    public void addAlbum (Album album) {
+        albums.add(album);
+    }
+
     @Override
     public String toString() {
-        return String.format("%d %s", id, name);
+        StringBuilder sb = new StringBuilder(String.valueOf(id));
+        sb.append(": ");
+        sb.append(name);
+        sb.append("\nAlbums:\n");
+        for(Album album : albums) {
+            sb.append("\t");
+            sb.append(album);
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 }
