6ce8d08862f2ca7914dbe67a269ad1c36fdb3ef9
Ken Laptop <samuel.ken.segawa@gmail.com>
2022-12-23 22:06:38 +0100

    Updated exceptions

--- app/src/main/java/se/iths/App.java
+++ app/src/main/java/se/iths/App.java
@@ -15,38 +15,31 @@ import static se.iths.Constants.*;
 public class App {
     public static void main(String[] args) throws SQLException {
         App app = new App();
-        app.load();
+        try {
+            app.load();
+        } catch (SQLException e) {
+            System.err.println(String.format("Error reading database %s", e.toString()));
+        }
     }
 
-    private void load() {
+    private void load() throws SQLException {
         Collection<Artist> artists = loadArtists();
         for(Artist artist : artists){
             System.out.println(artist);
         }
     }
 
-    private Collection<Artist> loadArtists() {
+    private Collection<Artist> loadArtists() throws SQLException {
         Collection<Artist> artists = new ArrayList<>();
-        Connection con = null;
-        ResultSet rs = null;
-        try {
-            con = DriverManager.getConnection(JDBC_CONNECTION, JDBC_USER, JDBC_PASSWORD);
-            rs = con.createStatement().executeQuery(SQL_SELECT_ALL_ARTISTS);
-            while (rs.next()) {
-                long id = rs.getLong(SQL_COL_ARTIST_ID);
-                String name = rs.getString(SQL_COL_ARTIST_NAME);
-                artists.add(new Artist(id, name));
-            }
-        } catch (SQLException e) {
-            System.err.println(String.format("Error reading database %s", e.toString()));
-        } finally {
-            try {
-                rs.close();
-            } catch (Exception ignore) {}
-            try {
-                con.close();
-            } catch (Exception ignore) {}
+        Connection con = DriverManager.getConnection(JDBC_CONNECTION, JDBC_USER, JDBC_PASSWORD);
+        ResultSet rs = con.createStatement().executeQuery(SQL_SELECT_ALL_ARTISTS);
+        while (rs.next()) {
+            long id = rs.getLong(SQL_COL_ARTIST_ID);
+            String name = rs.getString(SQL_COL_ARTIST_NAME);
+            artists.add(new Artist(id, name));
         }
+        rs.close();
+        con.close();
         return artists;
     }
 }
